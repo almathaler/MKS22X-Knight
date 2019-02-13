6805429508bf963c4acbc45e96ee81e7db6980af
almathaler <athaler00@stuy.edu>
2019-02-13 10:55:38 -0500

    made constructor and to string no tests tho

--- KnightBoard.java
+++ KnightBoard.java
@@ -1,8 +1,19 @@
 public class KnightBoard{
+  int[][] board;
   /*
   **@throws IllegalArgumentException when either parameter is negative.
   */
-  public KnightBoard(int startingRows,int startingCols){}
+  public KnightBoard(int startingRows,int startingCols) throws IllegalArgumentException{
+    if (startingRows <= 0 || startingCols <= 0){
+      throw new IllegalArgumentException("your board must have positive non-zero dimensions");
+    }
+    board = new int[startingRows][startingCols];
+    for (int r = 0; r<startingRows; r++){
+      for (int c = 0; c<startingRows; c++){
+        board[r][c] = 0;
+      }
+    }
+  }
 
       //Initialize the board to the correct size and make them all 0's
 
@@ -11,7 +22,36 @@ public class KnightBoard{
   **you get a blank board if you never called solve or
   **when there is no solution
   */
-  public String toString(){}
+  public String toString(){
+    int numSpaces = 0;
+    int temp = board.length * board[0].length;
+    while (temp > 0){
+       temp/=10;
+       numSpaces++;
+    }
+    String toReturn = "";
+    if (board[0][0] == 0){
+      for (int r =0; r<board.length; r++){
+        for (int c = 0; c<board[0].length; c++){
+          toReturn += "_";
+        }
+        toReturn += "\n";
+      }
+      return toReturn;
+    }
+    for (int r =0; r<board.length; r++){
+      for (int c = 0; c<board[0].length; c++){
+        if ((board[r][c] + "").length() < numSpaces){
+          for (int i = 0; i<(numSpaces - (board[r][c] + "").length()); i++){
+            toReturn += " ";
+          }
+        }
+        toReturn += board[r][c];
+      }
+      toReturn += "\n";
+    }
+    return toReturn;
+  }
 
   /*
   **@throws IllegalStateException when the board contains non-zero values.
@@ -27,6 +67,6 @@ public class KnightBoard{
   public int countSolutions(int startingRow, int startingCol){}
 
   //Suggestion:
-  //private boolean solveH(int row ,int col, int level) 
+  //private boolean solveH(int row ,int col, int level)
   // level is the # of the knight
 }
