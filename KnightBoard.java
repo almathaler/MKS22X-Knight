b09cf1476b4be728fa697918bb1a8da3dc3385e3
almathaler <athaler00@stuy.edu>
2019-02-15 22:57:55 -0500

    works almost instantly for 5,5 doesn't finish reasonably for 8,8, but works

--- KnightBoard.java
+++ KnightBoard.java
@@ -15,9 +15,9 @@ public class KnightBoard{
   }
   //have to check (r-2, c-1), (r-2, c+1), (r-1, c-2), (r+1, c-2), (r+2, c-1), (r+2, c+1), (r-1, c+2), (r+1, c+2))
   int[][] board;
-  ArrayList<move> moveList = new ArrayList<move>(8);
+  ArrayList<move> moveList = new ArrayList<move>(5);
   public static void main(String[] args){
-    KnightBoard example = new KnightBoard(8, 8);
+    KnightBoard example = new KnightBoard(5, 5);
     System.out.println(example.moveList);
     example.solve(0, 0);
     System.out.println(example.toString());
@@ -112,18 +112,7 @@ public class KnightBoard{
     if (startingRow<0 || startingRow>=board.length || startingCol<0 || startingCol>=board[0].length){
       throw new IllegalStateException("you can't solve from an out-of-bounds position!");
     }
-    for (int r = 0; r<board.length; r++){ //go through every spot startng at topleft. if you can solve from
-      for (int c = 0; c<board[0].length; c++){ //any of those position just return treu
-        System.out.println("***\nChecking if board can be solved from " + r + ", " + c);
-        if (solveH(r, c, 1)){
-          System.out.println("it can be");
-          return true;
-        }else{
-          clearBoard();
-        }
-      }
-    }
-    return false;
+    return solveH(startingRow, startingCol, 1);
   }
 
   private boolean clearBoard(){
@@ -136,32 +125,28 @@ public class KnightBoard{
   }
 
   private boolean solveH(int r, int c, int level){
-    int i = 0;
-    while (i<moveList.size()){
-      try{ //try block so things that are out of bounds are just skipped
-        int newR = r + moveList.get(i).rChange; //based on the changes in the list, make new coords for next call
-        int newC = c + moveList.get(i).cChange;
-        System.out.println("\n\nIn solveH, checking to see if board at " + r + ", " + c + " is available for spot " + level);
-        if (board[r][c] == 0 || (i > 0 && board[r][c] == level)){ //if the current value can have something placed on it
-          System.out.println("It is available, changing it to " + level);
-          //System.out.println(toString());
-          board[r][c] = level; //place the new number
-          System.out.println(toString());
-          if (level == board.length * board[0].length){ //if this number was the area, you're done and return true
-            return true;
-          }
-          System.out.println("Now checking moving by " + moveList.get(i).rChange + ", " + moveList.get(i).cChange);
-          if (solveH(newR, newC, level+1)){
-            return true;
-          }else{
-          //  board[newR][newC] = 0;
-          }//otherwise, after adding, just go to the next spot (determined by moveList) and see where you can assign the next val
-        }
-      }catch(ArrayIndexOutOfBoundsException e){
-        //don;t do anything just try the next one
+    if (board[r][c] == 0){ //if you can add
+      board[r][c] = level;
+      //System.out.println(r + ", " + c + " is free. setting that to  " + level);
+      //System.out.println(toString());
+      if (level == board.length * board[0].length){
+        return true; //if you added the lsat move, true
       }
-      i++; //while loop so that if you're checking a new spot, list will reset
+      for (int i = 0; i<moveList.size(); i++){ //if not, try all 8 moves
+        try{ //some will be outofbounds. if they are, j continue on w the loop
+          //System.out.println("\n***Now calling solveH in for loop to check " + (r+moveList.get(i).rChange) + ", " + (c+moveList.get(i).cChange));
+          if (solveH(r+moveList.get(i).rChange, c+moveList.get(i).cChange, level+1)){ //if any of the moves are valid
+            return true; //if you can keep adding until you get to the last, return true
+          }
+        }catch(ArrayIndexOutOfBoundsException e){
+          //don't do anything j let loop continue
+        }
+      }
+      //System.out.println("no possible move from here, so setting the board at " + r + ", " + c + " to 0.");
+      board[r][c] = 0;
+      //System.out.println(toString());
     }
+    return false; //if you can't add to this spot return false
     /*
     //have to check (r-2, c-1), (r-2, c+1), (r-1, c-2), (r+1, c-2), (r+2, c-1), (r+2, c+1), (r-1, c+2), (r+1, c+2))
     for (int i = 0; i<moveList.size(); i++){ //go through every possible move
@@ -190,7 +175,7 @@ public class KnightBoard{
     //System.out.println("No move possible from " + r + ", " + c);
     //System.out.println(toString());
     //clearBoard(); // if it didn't work, clear the board so that other calls won't be messed up
-    return false;
+    //return false;
   }
   /*
   **@throws IllegalStateException when the board contains non-zero values.
